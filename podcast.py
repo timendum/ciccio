b41693d2570bf94e3dcf791081a56197a0a24613
timendum <timedum@gmail.com>
2023-09-28 17:40:24 +0200

    Better xml feed

--- podcast.py
+++ podcast.py
@@ -33,7 +33,7 @@ def find_mp3() -> Puntata | None:
 def already_done(p: Puntata, outdir: str) -> bool:
     try:
         feed = feedendum.from_rss_file(path.join(outdir, "terzo-incomodo.xml"))
-        return p.title in feed.items[0].title
+        return feed.items and p.title in feed.items[0].title
     except OSError:
         return False
 
@@ -41,19 +41,31 @@ def already_done(p: Puntata, outdir: str) -> bool:
 def make_feed(p: Puntata, files: list[str], outdir: str) -> None:
     now = datetime.now()
     feed = feedendum.feed.Feed(
-        title="Il terzo incomodo",
+        title="Il terzo incomodo - podcast non ufficiale",
         url="https://www.deejay.it/programmi/il-terzo-incomodo/puntate/",
         description="Puntate del terzo incomodo, ma con solo gli spezzoni di parlato.",
         update=now + timedelta(minutes=30),
+        _data={
+            "image": {
+                "url": "https://www.omnycontent.com/d/playlist/60311b15-274a-4e3f-8ba9-ac3000834f37/1636480f-f515-4800-ad4b-b07500779d1f/72cf72a2-49cb-42ef-a619-b07500779d52/image.jpg?t=1694095271&size=Large"
+            }
+        },
     )
     for i, f in enumerate(files):
         feed.items.append(
             feedendum.feed.FeedItem(
                 title=f"{p.title} - {i + 1:0>2}",
                 url=f"{p.url}#{i:0>2}",
-                content=f"Spezzone {i + 1}/{(len(files) + 1)}\nDa {p.mp3}",
+                id=f"{p.url}#{i:0>2}",
+                content=f"Spezzone {i + 1} di {(len(files) + 1)}\nDa {p.mp3}",
                 update=now + timedelta(minutes=i),
-                _data={"enclosure": {"@url": f"./{f}", "@type": "audio/mpeg"}},
+                _data={
+                    "enclosure": {
+                        "@url": f"./{f}",
+                        "@type": "audio/mpeg",
+                        "@length": str(path.getsize(f)),
+                    }
+                },
             )
         )
         shutil.move(f, path.join(outdir, f))
